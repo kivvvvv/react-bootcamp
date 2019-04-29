7d4a3ed6ebab58d606150b499131c197e4b02750
kivvvvv <kivistheslayer@gmail.com>
2019-04-29 16:38:21 +0700

    Intro the DefaultProps

--- 03-props-and-more/index.js
+++ 03-props-and-more/index.js
@@ -11,8 +11,6 @@ class App extends React.Component {
       <div>
         <Hello
           to="Ringo"
-          from="Paul"
-          bangs={4}
           img="https://images.unsplash.com/photo-1536053341826-c373a78370b4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=3782&q=80"
         />
       </div>
