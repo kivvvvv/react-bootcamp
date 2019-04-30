d1f4f54c624cf3856d26c0671ca0c04b2445efab
kivvvvv <kivistheslayer@gmail.com>
2019-05-01 06:35:48 +0700

    Pokedex: change Pokecard styling

--- 05-pokedex-project/pokemon/src/Pokecard.js
+++ 05-pokedex-project/pokemon/src/Pokecard.js
@@ -6,12 +6,14 @@ class Pokecard extends Component {
     const { name, image, type, exp } = this.props;
     return (
       <figure className="Pokecard">
+        <div className="Pokecard-image-container">
+          <img
+            className="Pokecard-image"
+            src={image}
+            alt={`a pokemon name ${name}`}
+          />
+        </div>
         <header className="Pokecard-header">{name}</header>
-        <img
-          className="Pokecard-image"
-          src={image}
-          alt={`a pokemon name ${name}`}
-        />
         <figcaption className="Pokecard-type">Type: {type}</figcaption>
         <figcaption className="Pokecard-exp">EXP: {exp}</figcaption>
       </figure>
