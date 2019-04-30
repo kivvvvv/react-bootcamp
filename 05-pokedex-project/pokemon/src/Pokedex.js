dce98777de602493bd1e45fc1c04f26fa1c52132
kivvvvv <kivistheslayer@gmail.com>
2019-05-01 05:00:52 +0700

    Pokedex: changed randomed logic in Pokedex

--- 05-pokedex-project/pokemon/src/Pokedex.js
+++ 05-pokedex-project/pokemon/src/Pokedex.js
@@ -1,22 +1,16 @@
 import React, { Component } from "react";
 import Pokecard from "./Pokecard";
 import Pokemons from "./Pokemons";
-import { randomPokemon } from "./helpers";
+import { shuffleArray } from "./helpers";
 import "./Pokedex.css";
 
 class Pokedex extends Component {
   render() {
-    let randomedPokemons = [];
-    while (Pokemons.length > 0) {
-      const randomedPokemon = randomPokemon(Pokemons);
-      randomedPokemons.push(randomedPokemon);
-    }
-
     return (
       <section className="Pokedex">
         <h1 className="Pokedex-header">Pokedex</h1>
         <div className="Pokedex-grid">
-          {randomedPokemons.map(pokemon => {
+          {shuffleArray(Pokemons).map(pokemon => {
             return (
               <Pokecard
                 name={pokemon.name}
--- 05-pokedex-project/pokemon/src/helpers.js
+++ 05-pokedex-project/pokemon/src/helpers.js
@@ -1,6 +1,18 @@
-const randomPokemon = pokemons => {
-  let randomedIndex = Math.floor(Math.random() * pokemons.length);
-  return pokemons.splice(randomedIndex, 1)[0];
+// Shuffle function from http://stackoverflow.com/a/2450976
+const shuffleArray = array => {
+  let currentIndex = array.length,
+    temporaryValue,
+    randomIndex;
+
+  while (currentIndex !== 0) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
 };
 
-export { randomPokemon };
+export { shuffleArray };
