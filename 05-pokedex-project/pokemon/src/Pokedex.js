9d6b13a00da480a6803d054aba55201cc3119db2
kivvvvv <kivistheslayer@gmail.com>
2019-04-30 19:17:46 +0700

    Pokedex: init Pokedex styling

--- 05-pokedex-project/pokemon/src/Pokedex.js
+++ 05-pokedex-project/pokemon/src/Pokedex.js
@@ -1,18 +1,59 @@
 import React, { Component } from "react";
 import { Pokecard } from "./Pokecard";
+import "./Pokedex.css";
 
 class Pokedex extends Component {
   render() {
     return (
       <section className="Pokedex">
-        <Pokecard />
-        <Pokecard />
-        <Pokecard />
-        <Pokecard />
-        <Pokecard />
-        <Pokecard />
-        <Pokecard />
-        <Pokecard />
+        <Pokecard
+          name="Charmander"
+          image="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/4.png"
+          type="fire"
+          exp="62"
+        />
+        <Pokecard
+          name="Charmander"
+          image="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/4.png"
+          type="fire"
+          exp="62"
+        />
+        <Pokecard
+          name="Charmander"
+          image="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/4.png"
+          type="fire"
+          exp="62"
+        />
+        <Pokecard
+          name="Charmander"
+          image="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/4.png"
+          type="fire"
+          exp="62"
+        />
+        <Pokecard
+          name="Charmander"
+          image="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/4.png"
+          type="fire"
+          exp="62"
+        />
+        <Pokecard
+          name="Charmander"
+          image="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/4.png"
+          type="fire"
+          exp="62"
+        />
+        <Pokecard
+          name="Charmander"
+          image="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/4.png"
+          type="fire"
+          exp="62"
+        />
+        <Pokecard
+          name="Charmander"
+          image="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/4.png"
+          type="fire"
+          exp="62"
+        />
       </section>
     );
   }
