d374ab9c706223f99b37f9028dc1fe90c03aee77
kivvvvv <kivistheslayer@gmail.com>
2019-05-03 06:27:37 +0700

    Lottery: test App with Lottery

--- 08-react-state-patterns/lotto_exercise/src/App.js
+++ 08-react-state-patterns/lotto_exercise/src/App.js
@@ -1,8 +1,13 @@
 import React from "react";
+import Lottery from "./Lottery";
 import "./App.css";
 
 function App() {
-  return <div className="App" />;
+  return (
+    <div className="App">
+      <Lottery />
+    </div>
+  );
 }
 
 export default App;
