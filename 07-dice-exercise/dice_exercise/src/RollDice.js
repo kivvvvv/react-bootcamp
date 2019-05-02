c5c51126c69b963f6604c457c8d0ec765393415f
kivvvvv <kivistheslayer@gmail.com>
2019-05-02 14:15:58 +0700

    Dice: add roll on RollDice

--- 07-dice-exercise/dice_exercise/src/RollDice.js
+++ 07-dice-exercise/dice_exercise/src/RollDice.js
@@ -3,13 +3,34 @@ import Dice from "./Dice";
 import "./RollDice.css";
 
 class RollDice extends Component {
+  static defaultProps = {
+    faces: ["one", "two", "three", "four", "five", "six"]
+  };
+  constructor() {
+    super();
+    this.state = {
+      diceFaces: ["one", "one"]
+    };
+    this.roll = this.roll.bind(this);
+  }
+  roll() {
+    const { faces } = this.props;
+
+    this.setState({
+      diceFaces: this.state.diceFaces.map(
+        () => faces[Math.floor(Math.random() * faces.length)]
+      )
+    });
+  }
   render() {
     return (
       <div className="RollDice">
         <div className="RollDice-dices">
-          <Dice face="one" />
-          <Dice face="two" />
+          {this.state.diceFaces.map(face => (
+            <Dice face={face} />
+          ))}
         </div>
+        <button onClick={this.roll}>Roll Dice</button>
       </div>
     );
   }
