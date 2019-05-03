ff6a46fb71fe0962166d455c0ef0b1619db6f0dd
kivvvvv <kivistheslayer@gmail.com>
2019-05-03 08:49:27 +0700

    Lottery: test App render 2 Lottery

--- 08-react-state-patterns/lotto_exercise/src/App.js
+++ 08-react-state-patterns/lotto_exercise/src/App.js
@@ -5,7 +5,8 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <Lottery />
+      <Lottery title="Lotto" numBalls={6} maxNum={40} />
+      <Lottery title="Mini Daily" numBalls={4} maxNum={10} />
     </div>
   );
 }
