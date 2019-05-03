a020d28bfabffaf79e8ee5ff5a31af36d523732e
kivvvvv <kivistheslayer@gmail.com>
2019-05-03 07:19:25 +0700

    Lottery: add random Lottery on construct.

--- 08-react-state-patterns/lotto_exercise/src/Lottery.js
+++ 08-react-state-patterns/lotto_exercise/src/Lottery.js
@@ -2,14 +2,28 @@ import React, { Component } from "react";
 import LotteryBall from "./LotteryBall";
 
 class Lottery extends Component {
+  static defaultProps = {
+    title: "Lotto",
+    numBalls: 6,
+    maxNum: 40
+  };
+  constructor(props) {
+    super(props);
+
+    const nums = [];
+    const maxNum = this.props.maxNum;
+    for (let i = 0; i < this.props.numBalls; i++) {
+      const randNum = Math.floor(Math.random() * maxNum) + 1;
+      nums.push(randNum);
+    }
+    this.state = { nums: nums };
+  }
   render() {
     return (
       <div>
-        <LotteryBall num={5} />
-        <LotteryBall num={5} />
-        <LotteryBall num={5} />
-        <LotteryBall num={5} />
-        <LotteryBall num={5} />
+        {this.state.nums.map(num => (
+          <LotteryBall num={num} />
+        ))}
       </div>
     );
   }
