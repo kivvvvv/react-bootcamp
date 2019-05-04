cb44862ba7f0af8ec6e06bed8f70d6fb0626100e
kivvvvv <kivistheslayer@gmail.com>
2019-05-04 19:18:16 +0700

    Hangman: add key to button on Hangman

--- 11-hangman-exercise/hangman_exercise/src/Hangman.js
+++ 11-hangman-exercise/hangman_exercise/src/Hangman.js
@@ -46,6 +46,7 @@ class Hangman extends Component {
   generateButtons() {
     return "abcdefghijklmnopqrstuvwxyz".split("").map(ltr => (
       <button
+        key={ltr}
         value={ltr}
         onClick={this.handleGuess}
         disabled={this.state.guessed.has(ltr)}
@@ -58,11 +59,11 @@ class Hangman extends Component {
   /** render: render game */
   render() {
     return (
-      <div className='Hangman'>
+      <div className="Hangman">
         <h1>Hangman</h1>
         <img src={this.props.images[this.state.nWrong]} />
-        <p className='Hangman-word'>{this.guessedWord()}</p>
-        <p className='Hangman-btns'>{this.generateButtons()}</p>
+        <p className="Hangman-word">{this.guessedWord()}</p>
+        <p className="Hangman-btns">{this.generateButtons()}</p>
       </div>
     );
   }
