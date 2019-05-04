9fa6f88760f8198b0c0baef60b0945ea5d5ae004
kivvvvv <kivistheslayer@gmail.com>
2019-05-04 15:50:16 +0700

    Color-Boxes: refactor to adhere the DDF

--- 09-state-exercises/color_boxes_exercise/src/Box.js
+++ 09-state-exercises/color_boxes_exercise/src/Box.js
@@ -2,191 +2,20 @@ import React, { Component } from "react";
 import "./Box.css";
 
 export default class Box extends Component {
-  static defaultProps = {
-    colors: [
-      "AliceBlue",
-      "AntiqueWhite",
-      "Aqua",
-      "Aquamarine",
-      "Azure",
-      "Beige",
-      "Bisque",
-      "Black",
-      "BlanchedAlmond",
-      "Blue",
-      "BlueViolet",
-      "Brown",
-      "BurlyWood",
-      "CadetBlue",
-      "Chartreuse",
-      "Chocolate",
-      "Coral",
-      "CornflowerBlue",
-      "Cornsilk",
-      "Crimson",
-      "Cyan",
-      "DarkBlue",
-      "DarkCyan",
-      "DarkGoldenRod",
-      "DarkGray",
-      "DarkGrey",
-      "DarkGreen",
-      "DarkKhaki",
-      "DarkMagenta",
-      "DarkOliveGreen",
-      "Darkorange",
-      "DarkOrchid",
-      "DarkRed",
-      "DarkSalmon",
-      "DarkSeaGreen",
-      "DarkSlateBlue",
-      "DarkSlateGray",
-      "DarkSlateGrey",
-      "DarkTurquoise",
-      "DarkViolet",
-      "DeepPink",
-      "DeepSkyBlue",
-      "DimGray",
-      "DimGrey",
-      "DodgerBlue",
-      "FireBrick",
-      "FloralWhite",
-      "ForestGreen",
-      "Fuchsia",
-      "Gainsboro",
-      "GhostWhite",
-      "Gold",
-      "GoldenRod",
-      "Gray",
-      "Grey",
-      "Green",
-      "GreenYellow",
-      "HoneyDew",
-      "HotPink",
-      "IndianRed",
-      "Indigo",
-      "Ivory",
-      "Khaki",
-      "Lavender",
-      "LavenderBlush",
-      "LawnGreen",
-      "LemonChiffon",
-      "LightBlue",
-      "LightCoral",
-      "LightCyan",
-      "LightGoldenRodYellow",
-      "LightGray",
-      "LightGrey",
-      "LightGreen",
-      "LightPink",
-      "LightSalmon",
-      "LightSeaGreen",
-      "LightSkyBlue",
-      "LightSlateGray",
-      "LightSlateGrey",
-      "LightSteelBlue",
-      "LightYellow",
-      "Lime",
-      "LimeGreen",
-      "Linen",
-      "Magenta",
-      "Maroon",
-      "MediumAquaMarine",
-      "MediumBlue",
-      "MediumOrchid",
-      "MediumPurple",
-      "MediumSeaGreen",
-      "MediumSlateBlue",
-      "MediumSpringGreen",
-      "MediumTurquoise",
-      "MediumVioletRed",
-      "MidnightBlue",
-      "MintCream",
-      "MistyRose",
-      "Moccasin",
-      "NavajoWhite",
-      "Navy",
-      "OldLace",
-      "Olive",
-      "OliveDrab",
-      "Orange",
-      "OrangeRed",
-      "Orchid",
-      "PaleGoldenRod",
-      "PaleGreen",
-      "PaleTurquoise",
-      "PaleVioletRed",
-      "PapayaWhip",
-      "PeachPuff",
-      "Peru",
-      "Pink",
-      "Plum",
-      "PowderBlue",
-      "Purple",
-      "Red",
-      "RosyBrown",
-      "RoyalBlue",
-      "SaddleBrown",
-      "Salmon",
-      "SandyBrown",
-      "SeaGreen",
-      "SeaShell",
-      "Sienna",
-      "Silver",
-      "SkyBlue",
-      "SlateBlue",
-      "SlateGray",
-      "SlateGrey",
-      "Snow",
-      "SpringGreen",
-      "SteelBlue",
-      "Tan",
-      "Teal",
-      "Thistle",
-      "Tomato",
-      "Turquoise",
-      "Violet",
-      "Wheat",
-      "White",
-      "WhiteSmoke",
-      "Yellow",
-      "YellowGreen"
-    ]
-  };
-
   constructor(props) {
     super(props);
-
-    this.genColor = currentColor => {
-      let colorRange = this.props.colors;
-
-      if (currentColor) {
-        colorRange = this.props.colors.filter(color => color !== currentColor);
-      }
-
-      return colorRange[Math.floor(Math.random() * colorRange.length)];
-    };
-
-    this.state = {
-      currentColor: this.genColor(null)
-    };
+    this.handleChangeColor = this.handleChangeColor.bind(this);
   }
-
-  handleClick(e) {
-    const currentColor = e.target.style.backgroundColor;
-    this.setState({
-      currentColor: this.genColor(currentColor)
-    });
+  handleChangeColor() {
+    const { boxIndex, color } = this.props;
+    this.props.changeColor(boxIndex, color);
   }
-
   render() {
     return (
       <div
         className="Box"
-        onClick={e => {
-          this.handleClick(e);
-        }}
-        style={{ background: this.state.currentColor }}
+        style={{ background: this.props.color }}
+        onClick={this.handleChangeColor}
       />
     );
   }
--- 09-state-exercises/color_boxes_exercise/src/BoxContainer.js
+++ 09-state-exercises/color_boxes_exercise/src/BoxContainer.js
@@ -4,14 +4,192 @@ import "./BoxContainer.css";
 
 export default class BoxContainer extends Component {
   static defaultProps = {
-    nBox: 16
+    nBox: 16,
+    colors: [
+      "AliceBlue",
+      "AntiqueWhite",
+      "Aqua",
+      "Aquamarine",
+      "Azure",
+      "Beige",
+      "Bisque",
+      "Black",
+      "BlanchedAlmond",
+      "Blue",
+      "BlueViolet",
+      "Brown",
+      "BurlyWood",
+      "CadetBlue",
+      "Chartreuse",
+      "Chocolate",
+      "Coral",
+      "CornflowerBlue",
+      "Cornsilk",
+      "Crimson",
+      "Cyan",
+      "DarkBlue",
+      "DarkCyan",
+      "DarkGoldenRod",
+      "DarkGray",
+      "DarkGrey",
+      "DarkGreen",
+      "DarkKhaki",
+      "DarkMagenta",
+      "DarkOliveGreen",
+      "Darkorange",
+      "DarkOrchid",
+      "DarkRed",
+      "DarkSalmon",
+      "DarkSeaGreen",
+      "DarkSlateBlue",
+      "DarkSlateGray",
+      "DarkSlateGrey",
+      "DarkTurquoise",
+      "DarkViolet",
+      "DeepPink",
+      "DeepSkyBlue",
+      "DimGray",
+      "DimGrey",
+      "DodgerBlue",
+      "FireBrick",
+      "FloralWhite",
+      "ForestGreen",
+      "Fuchsia",
+      "Gainsboro",
+      "GhostWhite",
+      "Gold",
+      "GoldenRod",
+      "Gray",
+      "Grey",
+      "Green",
+      "GreenYellow",
+      "HoneyDew",
+      "HotPink",
+      "IndianRed",
+      "Indigo",
+      "Ivory",
+      "Khaki",
+      "Lavender",
+      "LavenderBlush",
+      "LawnGreen",
+      "LemonChiffon",
+      "LightBlue",
+      "LightCoral",
+      "LightCyan",
+      "LightGoldenRodYellow",
+      "LightGray",
+      "LightGrey",
+      "LightGreen",
+      "LightPink",
+      "LightSalmon",
+      "LightSeaGreen",
+      "LightSkyBlue",
+      "LightSlateGray",
+      "LightSlateGrey",
+      "LightSteelBlue",
+      "LightYellow",
+      "Lime",
+      "LimeGreen",
+      "Linen",
+      "Magenta",
+      "Maroon",
+      "MediumAquaMarine",
+      "MediumBlue",
+      "MediumOrchid",
+      "MediumPurple",
+      "MediumSeaGreen",
+      "MediumSlateBlue",
+      "MediumSpringGreen",
+      "MediumTurquoise",
+      "MediumVioletRed",
+      "MidnightBlue",
+      "MintCream",
+      "MistyRose",
+      "Moccasin",
+      "NavajoWhite",
+      "Navy",
+      "OldLace",
+      "Olive",
+      "OliveDrab",
+      "Orange",
+      "OrangeRed",
+      "Orchid",
+      "PaleGoldenRod",
+      "PaleGreen",
+      "PaleTurquoise",
+      "PaleVioletRed",
+      "PapayaWhip",
+      "PeachPuff",
+      "Peru",
+      "Pink",
+      "Plum",
+      "PowderBlue",
+      "Purple",
+      "Red",
+      "RosyBrown",
+      "RoyalBlue",
+      "SaddleBrown",
+      "Salmon",
+      "SandyBrown",
+      "SeaGreen",
+      "SeaShell",
+      "Sienna",
+      "Silver",
+      "SkyBlue",
+      "SlateBlue",
+      "SlateGray",
+      "SlateGrey",
+      "Snow",
+      "SpringGreen",
+      "SteelBlue",
+      "Tan",
+      "Teal",
+      "Thistle",
+      "Tomato",
+      "Turquoise",
+      "Violet",
+      "Wheat",
+      "White",
+      "WhiteSmoke",
+      "Yellow",
+      "YellowGreen"
+    ]
   };
-  render() {
-    let boxes = [];
-    while (boxes.length < this.props.nBox) {
-      boxes.push(<Box />);
-    }
 
-    return <div className="BoxContainer">{boxes}</div>;
+  constructor(props) {
+    super(props);
+
+    this.genColor = currentColor => {
+      let colorRange = this.props.colors;
+
+      if (currentColor) {
+        colorRange = this.props.colors.filter(color => color !== currentColor);
+      }
+
+      return colorRange[Math.floor(Math.random() * colorRange.length)];
+    };
+
+    this.state = {
+      currentColors: Array.from({ length: this.props.nBox }).map(() =>
+        this.genColor(null)
+      )
+    };
+    this.changeColor = this.changeColor.bind(this);
+  }
+  changeColor(currentBoxIndex, currentColor) {
+    this.setState(state => {
+      const currentColors = [...state.currentColors];
+      currentColors[currentBoxIndex] = this.genColor(currentColor);
+      return { currentColors: currentColors };
+    });
+  }
+  render() {
+    return (
+      <div className="BoxContainer">
+        {this.state.currentColors.map((color, index) => (
+          <Box boxIndex={index} color={color} changeColor={this.changeColor} />
+        ))}
+      </div>
+    );
   }
 }
