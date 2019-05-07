e7ca3410f416a70568834e527034f9443d3fa7cd
kivvvvv <kivistheslayer@gmail.com>
2019-05-07 21:10:31 +0700

    Todo-App: init Todo, TodoList and App

---
+++ 15-todo-app/todo_app/src/TodoList.js
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+import Todo from "./Todo";
+
+export default class TodoList extends Component {
+  constructor() {
+    super();
+    this.state = {
+      todos: [{ id: 1, text: "test1" }, { id: 2, text: "test2" }]
+    };
+  }
+  renderTodo() {
+    return this.state.todos.map(todo => <Todo id={todo.id} text={todo.text} />);
+  }
+  render() {
+    return (
+      <section>
+        <header>
+          <h1>Todo List!</h1>
+          <h2>A Simple React Todo List App</h2>
+        </header>
+        <div>
+          {this.state.todos.length > 0 ? (
+            this.renderTodo()
+          ) : (
+            <p>There is no todo.</p>
+          )}
+        </div>
+      </section>
+    );
+  }
+}
