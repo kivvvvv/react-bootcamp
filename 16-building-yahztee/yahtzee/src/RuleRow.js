ade4d175ec7a9c2c9e2b2ad6568a343ee7b2ae0e
kivvvvv <kivistheslayer@gmail.com>
2019-05-09 20:42:03 +0700

    Yahtzee: fix re-scoring

--- 16-building-yahztee/yahtzee/src/RuleRow.js
+++ 16-building-yahztee/yahtzee/src/RuleRow.js
@@ -1,14 +1,23 @@
-import React, { Component } from 'react';
-import './RuleRow.css'
+import React, { Component } from "react";
+import "./RuleRow.css";
 
 class RuleRow extends Component {
   render() {
+    const { doScore, name, score } = this.props;
+    if (score === undefined) {
+      return (
+        <tr className="RuleRow RuleRow-active" onClick={doScore}>
+          <td className="RuleRow-name">{name}</td>
+          <td className="RuleRow-score">{score}</td>
+        </tr>
+      );
+    }
     return (
-      <tr className="RuleRow RuleRow-active" onClick={this.props.doScore}>
-        <td className="RuleRow-name">{this.props.name}</td>
-        <td className="RuleRow-score">{this.props.score}</td>
+      <tr className="RuleRow RuleRow-disabled">
+        <td className="RuleRow-name">{name}</td>
+        <td className="RuleRow-score">{score}</td>
       </tr>
-    )
+    );
   }
 }
 
