80f02450f9951534c13ea6f5e2ec948db3813f43
kivvvvv <kivistheslayer@gmail.com>
2019-05-19 11:43:55 +0700

    Dad-Jokes: add Joke emoji condition

--- 19-dad-jokes-project/dad-jokes_project/src/Joke.js
+++ 19-dad-jokes-project/dad-jokes_project/src/Joke.js
@@ -12,6 +12,34 @@ export default class Joke extends Component {
   handleDownvoteJoke() {
     this.props.downvoteJoke(this.props.jokeId);
   }
+  renderEmoji() {
+    const totalVote = this.props.totalVote;
+    const emojies = ["Very Bad", "Bad", "Normal", "Good", "Very Good"];
+    let selectedEmoji;
+
+    switch (true) {
+      case totalVote >= 6:
+        selectedEmoji = emojies[4];
+        break;
+      case totalVote < 6 && totalVote >= 3:
+        selectedEmoji = emojies[3];
+        break;
+      case totalVote < 3 && totalVote >= -2:
+        selectedEmoji = emojies[2];
+        break;
+      case totalVote <= -3 && totalVote >= -5:
+        selectedEmoji = emojies[1];
+        break;
+      case totalVote <= -6:
+        selectedEmoji = emojies[0];
+        break;
+      default:
+        selectedEmoji = emojies[0];
+        break;
+    }
+
+    return <i>{selectedEmoji}</i>;
+  }
   render() {
     return (
       <div>
@@ -19,6 +47,7 @@ export default class Joke extends Component {
         <span>{this.props.totalVote}</span>
         <button onClick={this.handleDownvoteJoke}>Downvote</button>
         <li>{this.props.jokeMsg}</li>
+        {this.renderEmoji()}
       </div>
     );
   }
