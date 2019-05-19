50bcf4b7be39707c10b838cf9a11c57f7a82f1b3
kivvvvv <kivistheslayer@gmail.com>
2019-05-19 10:09:30 +0700

    Dad-Jokes: add up/down voting Joke

--- 19-dad-jokes-project/dad-jokes_project/src/Joke.js
+++ 19-dad-jokes-project/dad-jokes_project/src/Joke.js
@@ -1,9 +1,23 @@
 import React, { Component } from "react";
 
 export default class Joke extends Component {
+  constructor(props) {
+    super(props);
+    this.handleUpvoteJoke = this.handleUpvoteJoke.bind(this);
+    this.handleDownvoteJoke = this.handleDownvoteJoke.bind(this);
+  }
+  handleUpvoteJoke() {
+    this.props.upvoteJoke(this.props.jokeId);
+  }
+  handleDownvoteJoke() {
+    this.props.downvoteJoke(this.props.jokeId);
+  }
   render() {
     return (
       <div>
+        <button onClick={this.handleUpvoteJoke}>Upvote</button>
+        <span>{this.props.totalVote}</span>
+        <button onClick={this.handleDownvoteJoke}>Downvote</button>
         <li>{this.props.jokeMsg}</li>
       </div>
     );
--- 19-dad-jokes-project/dad-jokes_project/src/JokeList.js
+++ 19-dad-jokes-project/dad-jokes_project/src/JokeList.js
@@ -9,6 +9,8 @@ export default class JokeList extends Component {
     this.state = {
       jokes: []
     };
+    this.upvoteJoke = this.upvoteJoke.bind(this);
+    this.downvoteJoke = this.downvoteJoke.bind(this);
   }
   getJokeJSON() {
     return fetch(JOKE_API, { headers: { Accept: "application/json" } }).then(
@@ -22,12 +24,29 @@ export default class JokeList extends Component {
 
     return Promise.all(Array.from({ length: iJoke }).map(this.getJokeJSON));
   }
+  upvoteJoke(id) {
+    this.setState(state => {
+      const currentJoke = state.jokes.find(joke => joke.id === id);
+      if (currentJoke) currentJoke.totalVote++;
+      return state;
+    });
+  }
+  downvoteJoke(id) {
+    this.setState(state => {
+      const currentJoke = state.jokes.find(joke => joke.id === id);
+      if (currentJoke) currentJoke.totalVote--;
+      return state;
+    });
+  }
   componentDidMount() {
     this.getJokes().then(
       function onFulfilledJokes(jokes) {
         this.setState(state => {
           return {
-            jokes: [...state.jokes, ...jokes]
+            jokes: [
+              ...state.jokes,
+              ...jokes.map(joke => ({ ...joke, totalVote: 0 }))
+            ]
           };
         });
       }.bind(this)
@@ -38,7 +57,14 @@ export default class JokeList extends Component {
       <div>
         <ul>
           {this.state.jokes.map(joke => (
-            <Joke jokeMsg={joke.joke} />
+            <Joke
+              key={joke.id}
+              jokeId={joke.id}
+              totalVote={joke.totalVote}
+              jokeMsg={joke.joke}
+              upvoteJoke={this.upvoteJoke}
+              downvoteJoke={this.downvoteJoke}
+            />
           ))}
         </ul>
       </div>
