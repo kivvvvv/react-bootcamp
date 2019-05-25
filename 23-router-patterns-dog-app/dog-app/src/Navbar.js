c93cd2e89894651e0498c2732bbdabdc4b1ffd12
kivvvvv <kivistheslayer@gmail.com>
2019-05-25 15:12:50 +0700

    Dog-App: add Navbar styling

--- 23-router-patterns-dog-app/dog-app/src/Navbar.js
+++ 23-router-patterns-dog-app/dog-app/src/Navbar.js
@@ -2,16 +2,23 @@ import React, { Component } from "react";
 import { NavLink } from "react-router-dom";
 import uuid from "uuid/v4";
 
+import "./Navbar.css";
+
 export default class Navbar extends Component {
   render() {
     const dogsProps = this.props.routesInfoProps.dogs;
 
     return (
-      <nav>
-        <ul>
+      <nav className="Navbar">
+        <ul className="Navbar__lists">
           {dogsProps.map(dog => (
-            <li key={uuid()}>
-              <NavLink to={`/dogs/${dog.name}`}>{dog.name}</NavLink>
+            <li key={uuid()} className="Navbar__lists__item">
+              <NavLink
+                activeClassName="lists__item__link--active"
+                to={`/dogs/${dog.name}`}
+              >
+                {dog.name}
+              </NavLink>
             </li>
           ))}
         </ul>
