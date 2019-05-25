4dca79cd7b49a1da260bada0cb02141ec8505f03
kivvvvv <kivistheslayer@gmail.com>
2019-05-25 14:28:12 +0700

    Dog-App: add Links to DogList

--- 23-router-patterns-dog-app/dog-app/src/DogList.js
+++ 23-router-patterns-dog-app/dog-app/src/DogList.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 export default class DogList extends Component {
   render() {
@@ -6,12 +7,20 @@ export default class DogList extends Component {
     const { dogs } = this.props;
 
     return (
-      <div>
-        <ul>
+      <div className="DogList">
+        <h1>Click a Dog!</h1>
+        <div className="DogList__lists">
           {dogs.map(dog => (
-            <li>{dog.name}</li>
+            <figure className="DogList__figure">
+              <div className="DogList__figure__img">
+                <img alt={`a dog name ${dog.name}`} src={dog.src} />
+              </div>
+              <figcaption className="DogList__figure__name">
+                <Link to={`/dogs/${dog.name}`}>{dog.name}</Link>
+              </figcaption>
+            </figure>
           ))}
-        </ul>
+        </div>
       </div>
     );
   }
