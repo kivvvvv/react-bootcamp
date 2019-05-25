e198f8be57f6c8dba67433a0738a17af3e7843ef
kivvvvv <kivistheslayer@gmail.com>
2019-05-25 15:19:59 +0700

    Dog-App: add DogDetails styling

--- 23-router-patterns-dog-app/dog-app/src/DogDetails.js
+++ 23-router-patterns-dog-app/dog-app/src/DogDetails.js
@@ -1,19 +1,19 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 
+import "./DogDetails.css";
+
 export default class DogDetails extends Component {
   render() {
     const { name, age, src, facts } = this.props;
     return (
       <div className="DogDetails">
         <figure>
-          <img
-            className="DogDetails__img"
-            alt={`a dog name ${name}`}
-            src={src}
-          />
+          <div className="DogDetails__img-container">
+            <img alt={`a dog name ${name}`} src={src} />
+          </div>
           <figcaption className="DogDetails__name">{name}</figcaption>
-          <figcaption className="DogDetails__age">{age}</figcaption>
+          <figcaption className="DogDetails__age">{age} years old</figcaption>
           {facts.map(fact => (
             <figcaption className="DogDetails__facts">{fact}</figcaption>
           ))}
