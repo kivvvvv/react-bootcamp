75bfa30f7daf81e3629b0fa850b564b1c5ab76bd
kivvvvv <kivistheslayer@gmail.com>
2019-05-25 15:25:14 +0700

    Dog-App: add DogList styling

--- 23-router-patterns-dog-app/dog-app/src/DogList.js
+++ 23-router-patterns-dog-app/dog-app/src/DogList.js
@@ -2,17 +2,19 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import uuid from "uuid/v4";
 
+import "./DogList.css";
+
 export default class DogList extends Component {
   render() {
     const { dogs } = this.props;
 
     return (
       <div className="DogList">
-        <h1>Click a Dog!</h1>
+        <h1 className="DogList__header">Click a Dog!</h1>
         <div className="DogList__lists">
           {dogs.map(dog => (
             <figure key={uuid()} className="DogList__figure">
-              <div className="DogList__figure__img">
+              <div className="DogList__figure__img-container">
                 <img alt={`a dog name ${dog.name}`} src={dog.src} />
               </div>
               <figcaption className="DogList__figure__name">
