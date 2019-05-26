5b1462b71cffd36b1b17aa0e4d215603cdeef406
kivvvvv <kivistheslayer@gmail.com>
2019-05-26 10:18:45 +0700

    Colors-App: added Palette

--- react-colors/colors-app/src/App.js
+++ react-colors/colors-app/src/App.js
@@ -1,7 +1,13 @@
 import React, { Component } from "react";
+import Palette from "./Palette";
+import seedColors from "./seedColors";
 
 export default class App extends Component {
   render() {
-    return <div />;
+    return (
+      <div>
+        <Palette {...seedColors[4]} />
+      </div>
+    );
   }
 }
