1cd671ad32c6b2b0d92e7b2e170320bd89464327
kivvvvv <kivistheslayer@gmail.com>
2019-05-27 15:58:11 +0700

    186# Colors-App: added react-router-dom and Route to Palette

--- react-colors/colors-app/src/App.js
+++ react-colors/colors-app/src/App.js
@@ -2,13 +2,27 @@ import React, { Component } from "react";
 import Palette from "./Palette";
 import seedColors from "./seedColors";
 import { generatePalette } from "./colorHelpers";
+import { Switch, Route } from "react-router-dom";
 
 export default class App extends Component {
+  findPalette(id) {
+    return seedColors.find(palette => palette.id === id);
+  }
+
   render() {
     return (
-      <div>
-        <Palette palette={generatePalette(seedColors[4])} />
-      </div>
+      <Switch>
+        <Route exact path="/" render={() => <h1>PALETTE LIST GOES HERE</h1>} />
+        <Route
+          exact
+          path="/palette/:id"
+          render={({ match }) => (
+            <Palette
+              palette={generatePalette(this.findPalette(match.params.id))}
+            />
+          )}
+        />
+      </Switch>
     );
   }
 }
