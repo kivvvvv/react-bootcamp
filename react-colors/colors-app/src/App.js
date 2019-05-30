c3acb90a3d3bd0919279baf334b8cb7db5d351b8
kivvvvv <kivistheslayer@gmail.com>
2019-05-31 06:57:46 +0700

    207# Colors-App: added NewPaletteForm and Link

--- react-colors/colors-app/src/App.js
+++ react-colors/colors-app/src/App.js
@@ -4,7 +4,8 @@ import seedColors from "./seedColors";
 import { generatePalette } from "./colorHelpers";
 import { Switch, Route } from "react-router-dom";
 import PaletteList from "./PaletteList";
-import SingleColorPalette from './SingleColorPalette'
+import SingleColorPalette from "./SingleColorPalette";
+import NewPaletteForm from "./NewPaletteForm";
 
 export default class App extends Component {
   findPalette(id) {
@@ -21,6 +22,7 @@ export default class App extends Component {
             <PaletteList palettes={seedColors} history={history} />
           )}
         />
+        <Route exact path="/palette/new" render={() => <NewPaletteForm />} />
         <Route
           exact
           path="/palette/:id"
@@ -35,7 +37,9 @@ export default class App extends Component {
           path="/palette/:paletteId/:colorId"
           render={({ match }) => (
             <SingleColorPalette
-              palette={generatePalette(this.findPalette(match.params.paletteId))}
+              palette={generatePalette(
+                this.findPalette(match.params.paletteId)
+              )}
               colorId={match.params.colorId}
             />
           )}
--- react-colors/colors-app/src/styles/PaletteListStyles.js
+++ react-colors/colors-app/src/styles/PaletteListStyles.js
@@ -17,7 +17,11 @@ export default {
     display: "flex",
     width: "100%",
     justifyContent: "space-between",
-    color: "white"
+    alignItems: "center",
+    color: "white",
+    "& a": {
+      color: "inherit"
+    }
   },
   palettes: {
     boxSizing: "border-box",
