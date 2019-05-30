4d17062fdee91057081d71b40812be3404622d9c
kivvvvv <kivistheslayer@gmail.com>
2019-05-30 07:40:36 +0700

    195# Colors-App: added Route/Link to ColorBox

--- react-colors/colors-app/src/App.js
+++ react-colors/colors-app/src/App.js
@@ -29,6 +29,11 @@ export default class App extends Component {
             />
           )}
         />
+        <Route
+          exact
+          path="/palette/:paletteId/:colorId"
+          render={() => <h1>SINGLE COLOR PAGE!</h1>}
+        />
       </Switch>
     );
   }
